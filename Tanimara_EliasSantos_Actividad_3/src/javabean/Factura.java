52e87b4f2ef515e2afebc85a4be5e3e42686ed24
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-01-27 14:47:47 +0100

    Creada clase Factura

---
+++ Tanimara_EliasSantos_Actividad_3/src/javabean/Factura.java
@@ -0,0 +1,12 @@
+package javabean;
+
+import java.time.LocalDate;
+
+public class Factura {
+	
+	private String codigoFactura;
+	private String descripcion;
+	private LocalDate fechaFactura;
+	private Pedido pedido;
+
+}
